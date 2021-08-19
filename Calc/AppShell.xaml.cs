22b0c15ed3b859eba6732439a2875422be900e0b
flippflopp <tremtos@gmail.com>
2021-08-19 20:24:33 +0300

    first commit

---
+++ Calc/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Calc.ViewModels;
+using Calc.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Calc
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ Calc/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Calc.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Calc.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
